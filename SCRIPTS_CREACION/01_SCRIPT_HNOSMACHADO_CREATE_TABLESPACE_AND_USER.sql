ba2c98382ceb65144a1704f34fe76a48c660f05a
nandimarqz <nandimarquez31@gmail.com>
2022-05-04 13:06:50 +0200

    CORRECION_TABLAS_Y_CONSTRAINTS

--- SCRIPTS_CREACION/01_SCRIPT_HNOSMACHADO_CREATE_TABLESPACE_AND_USER.sql
+++ SCRIPTS_CREACION/01_SCRIPT_HNOSMACHADO_CREATE_TABLESPACE_AND_USER.sql
@@ -1,6 +1,12 @@
+------------------------------------------------------
+-- Autor       : IES HERMANOS MACHADO
+-- Descripci�n : Script CREACION TABLESPACE Y USUARIO - PROYECTO BBDD
+-- Responsables : Fernando M�rquez Rodr�guez , Rafael Jos� Ossorio Lop�z, Victor Carrasco Artacho, Carlos Gonz�lez Ruiz, Gabriel Rodr�guez F�lix 
+------------------------------------------------------
+
 /*CREAMOS EL TABLESPACE CON EL SYS PARA LUEGO PONERSELO AL USUARIO*/
 CREATE TABLESPACE PROYECTO_BBDD_DUAL_DATOS DATAFILE
-'F:\bbdd\Oracle\product\18.0.0\oradata\XE\proyecto_bbdd_dual_datos.dbf'
+'F:\bbdd\Oracle\product\18.0.0\oradata\XE\proyecto_bbdd_dual_datos2.dbf'
 SIZE 100M 
 AUTOEXTEND ON NEXT 100M MAXSIZE UNLIMITED;
 
