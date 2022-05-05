70f685cfbb3bf4de9852fee124a3718cf89de782
rafa <rafaeljoseossoriolopez@gmail.com>
2022-05-05 13:48:02 +0200

    Arreglo de tildes

--- SCRIPTS_CREACION/01_SCRIPT_HNOSMACHADO_CREATE_TABLESPACE_AND_USER.sql
+++ SCRIPTS_CREACION/01_SCRIPT_HNOSMACHADO_CREATE_TABLESPACE_AND_USER.sql
@@ -6,8 +6,7 @@
 
 /*CREAMOS EL TABLESPACE CON EL SYS PARA LUEGO PONERSELO AL USUARIO*/
 CREATE TABLESPACE PROYECTO_BBDD_DUAL_DATOS DATAFILE
---'F:\bbdd\Oracle\product\18.0.0\oradata\XE\proyecto_bbdd_dual_datos2.dbf'
-'E:\Oracle\oradata\XE\proyecto_bbdd_dual_datos2.dbf'
+'F:\bbdd\Oracle\product\18.0.0\oradata\XE\proyecto_bbdd_dual_datos2.dbf'
 SIZE 100M 
 AUTOEXTEND ON NEXT 100M MAXSIZE UNLIMITED;
 
